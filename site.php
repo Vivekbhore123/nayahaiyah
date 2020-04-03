79dab17ec99975ed431ae212bb9015b03d13a87a
Vivek <vivekbhore50@gmail.com>
2020-04-03 13:49:39 +0530

    site file added

---
+++ site.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       /*echo("Hello world");
+       echo "<hr>";
+       $charName="Raghav";
+       $charAge="40";
+       echo "<h1>Hello world</h1>";
+       echo "There was a guy named $charName<br>";
+       echo "He was $charAge years old<br>";
+       $charName="Billo rani";
+       echo "He was happy with his name ie $charName<br>";
+       echo "But he didn't liked his age ie $charAge<br>";*/
+       
+     ?>
+</body>
+</html>
